fcec3c50c6ad64f6e0c15acefd496c8a83ba401d
Florian Trube <flo.trube@gmail.com>
2018-11-04 15:57:44 +0100

    using UnityEngine.UI and public GameObjects and public Text and enum PageState

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject StartPage;
+    public GameObject GameOverPage;
+    public GameObject CountdownPage;
+    public Text ScoreText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+}
