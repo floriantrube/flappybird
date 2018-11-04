ce10d3d83ce97183fe1f4d9e1567f125e64c1861
Florian Trube <flo.trube@gmail.com>
2018-11-04 16:17:58 +0100

    Gamemanager und methoden zugewisen

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
     public GameObject StartPage;
     public GameObject GameOverPage;
     public GameObject CountdownPage;
@@ -16,5 +22,57 @@ public class GameManager : MonoBehaviour {
         Start,
         GameOver,
         Countdown
+    }
+
+    int score = 0;
+    bool gameOver = false;
+
+    public bool GameOver { get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(false);
+                CountdownPage.SetActive(false);
+                break;
+
+            case PageState.Start:
+                StartPage.SetActive(true);
+                GameOverPage.SetActive(false);
+                CountdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(true);
+                CountdownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(false);
+                CountdownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        //activated when the replay button is hit
+        OnGameOverConfirmed(); //event
+        ScoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+
+    public void StartGame()
+    {
+        //activated when playbutton is hit
+        SetPageState(PageState.Countdown);
     }
 }
