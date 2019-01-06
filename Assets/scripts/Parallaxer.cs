ff7ae9e0fe59d8121eb36652f6faf13308e9a7c6
Florian Trube <flo.trube@gmail.com>
2019-01-06 20:03:02 +0100

    PoolObject done

--- Assets/scripts/Parallaxer.cs
+++ Assets/scripts/Parallaxer.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Parallaxer : MonoBehaviour {
 
     class PoolObject {
-    
+        public Transform transform;
+        public bool inUse;
+        public PoolObject(Transform t) { transform = t; }
+        public void Use() { inUse = true;  }
+        public void Dispose() { inUse = false;  }
 }
 }
